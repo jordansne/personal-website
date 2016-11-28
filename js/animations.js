8eafee9fbbe51d1a0dacf114dc1d04eef76a6fb4
Jordan Sne <jordansne@gmail.com>
2016-11-28 01:42:37 -0500

    Cut code & add sections by DOM elements

--- js/animations.js
+++ js/animations.js
@@ -5,67 +5,49 @@
  * License: MIT
  */
 
-class Content {
+const PAGE_SCROLL_EVENTS = 'scrollend mousedown mousewheel wheel keyup touchmove DOMMouseScroll resize';
 
-    constructor(contentID, nextContent) {
+class SectionAnimation {
+
+    constructor(contentID) {
         this.contentID = '#' + contentID;
         this.contentLinkID = this.contentID + '_link';
-        this.nextContent = nextContent;
 
         $(this.contentID).css("opacity", 0);
         this.addScrollEvent();
+        this.registerFadeEvent();
     }
 
     /* Registers navbar buttons to their respective scroll events. */
     addScrollEvent() {
-        const self = this;
         const page = $('html, body');
-
         const stopFunction = function() { page.stop(); };
 
-        $(self.contentLinkID).click(function() {
+        $(this.contentLinkID).click(() => {
             page.animate({
-                scrollTop: $(self.contentID).offset().top - 75
-            }, 750, function() {
-                page.off('scroll mousedown mousewheel wheel keyup touchmove DOMMouseScroll resize', stopFunction);
+                scrollTop: $(this.contentID).offset().top - 75
+            }, 750, () => {
+                page.off(PAGE_SCROLL_EVENTS, stopFunction);
                 page.trigger('clicklink');
             });
 
-            page.one('scroll mousedown mousewheel wheel keyup touchmove DOMMouseScroll resize', stopFunction);
+            page.one(PAGE_SCROLL_EVENTS, stopFunction);
         });
     }
 
     /* Show content immediately if in view or register to an event handler if not. */
-    readyToShow() {
-        const self = this;
+    registerFadeEvent() {
         const page = $('html, body');
 
-        const animate = function() {
-            $(self.contentID).animate({
+        $(this.contentID).one('show', () => {
+            $(this.contentID).animate({
                 opacity: 1
-            }, 400, function() {
-                if (self.nextContent !== null) {
-                    self.nextContent.readyToShow();
-                }
-            });
-        };
+            }, 400);
+        });
 
-        if (self.isInView(self.contentID)) {
-            animate();
-        } else {
-            $(self.contentID).one('show', animate);
-            self.addShowEvent();
-        }
-    }
-
-    /* Adds event handler for scrolling to check for in-view content. */
-    addShowEvent() {
-        const self = this;
-        const page = $('html, body');
-
-        page.on('ready scroll mousedown mousewheel wheel keyup touchmove DOMMouseScroll resize clicklink', function() {
-            if (self.isInView(self.contentID)) {
-                $(self.contentID).trigger('show');
+        page.on(PAGE_SCROLL_EVENTS + ' clicklink', () => {
+            if (this.isInView(this.contentID)) {
+                $(this.contentID).trigger('show');
             }
         });
     }
@@ -83,19 +65,19 @@ class Content {
 }
 
 $(document).ready(function() {
-    const page = $('html, body');
 
-    const contactContent = new Content('contact', null);
-    const webPortfolioContent = new Content('web_portfolio', contactContent);
-    const projectsContent = new Content('projects', webPortfolioContent);
-    const aboutContent = new Content('about', projectsContent);
+    const sectionAnimations = [];
 
-    // Ensure page is at top before starting show animations
-    page.animate({
+    // Add all sections to the animation array
+    const sections = document.getElementsByTagName("section");
+    for (let i = 0; i < sections.length; i++) {
+        sectionAnimations.push(new SectionAnimation(sections[i].id));
+    }
+
+    // Ensure page is at top before starting show animations (in case of refresh)
+    $('html, body').animate({
         scrollTop: 0
-    }, 300, function() {
-        aboutContent.readyToShow();
-    });
+    }, 300);
 
     // Fade in "Jordan Sne |", and fade in arrow when finished.
     $('#header_animate, #arrow').css("opacity", 0);
